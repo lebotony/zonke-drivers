823c385900ae1ad8e8360d105f05542b1b3f54b2
lebohang <lebomdlongwa@gmail.com>
2025-10-10 11:33:59 +0200

    fix: delete babel

--- frontend/src/helpers/locations.ts
+++ frontend/src/helpers/locations.ts
@@ -1,41 +1,36 @@
-import { useCallback } from "react";
-
 import axios from "axios";
 import { debounce } from "lodash";
-import { MAPBOX_TOKEN } from "@env";
 
-  export const fetchSuggestions = useCallback(
-    debounce((text: string, onSetResults, onOpen) => {
-      if (text.length < 3) {
+  export const fetchSuggestions = debounce((text: string, onSetResults, onOpen) => {
+    const mapboxToken = process.env.EXPO_PUBLIC_MAPBOX_TOKEN;
+
+    if (text.length < 3) {
+      onSetResults([]);
+      return;
+    }
+
+    axios
+      .get(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          text
+        )}.json`,
+        {
+          params: {
+            access_token: mapboxToken,
+            autocomplete: true,
+            limit: 5,
+          },
+        }
+      )
+      .then((res) => {
+        const places = res.data.features.map(
+          (feature: Record<string, any>) => feature.place_name
+        );
+        onSetResults(places);
+        onOpen(true);
+      })
+      .catch((err) => {
+        console.error("Mapbox fetch error:", err.message);
         onSetResults([]);
-        return;
-      }
-
-      axios
-        .get(
-          `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-            text
-          )}.json`,
-          {
-            params: {
-              access_token: MAPBOX_TOKEN,
-              autocomplete: true,
-              limit: 5,
-            },
-          }
-        )
-        .then((res) => {
-          const places = res.data.features.map(
-            (feature: Record<string, any>) => feature.place_name
-          );
-          console.log("FFFFFFFFFFFFFFFFFFFFFFFFF", places);
-          onSetResults(places);
-          onOpen(true);
-        })
-        .catch((err) => {
-          console.error("Mapbox fetch error:", err.message);
-          onSetResults([]);
-        });
-    }, 400),
-    []
-  );
+      });
+  }, 400)
